94dbcc93690bd005e4e0de6ebf9895ec55f58a6c
benjim <bencrawford101@gmail.com>
2016-01-30 07:06:05 +0000

    Added ritual mechanic, item spawning and collection

---
+++ Jampire-Knights-GGJ2016/Assets/_Scripts/Item.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour
+{
+    bool collected = false;
+    float timeSinceCollection = 0;
+
+	void Start()
+    {
+	
+	}
+	
+	void Update()
+    {
+	    if (collected)
+        {
+            transform.position += Vector3.up * 0.5f * Time.deltaTime;
+            transform.Rotate(transform.up, 90 * Time.deltaTime);
+            timeSinceCollection += Time.deltaTime;
+            if (timeSinceCollection >= 2.5f)
+            {
+                Destroy(gameObject);
+            }
+        }
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            if (!collected)
+            {
+                collected = true;
+            }
+        }
+    }
+}
