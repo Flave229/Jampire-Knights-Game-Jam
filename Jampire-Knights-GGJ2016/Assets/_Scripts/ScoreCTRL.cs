2563633c6230818faca7b5c422364d1aac602f48
Mike <ymike2045@gmail.com>
2016-01-30 01:01:12 +0000

    A basic untested score controller.

---
+++ Jampire-Knights-GGJ2016/Assets/_Scripts/ScoreCTRL.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreCTRL : MonoBehaviour {
+	
+	private int _scoreDisp;
+	private float _scoreReal;
+
+	public Text _scoreText;
+
+	// Use this for initialization
+	void Start () {
+		_scoreDisp = 0;
+		_scoreReal = 0;
+		_scoreText = "Score: 000";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_scoreDisp += Time.deltaTime;
+		_scoreReal = (int) (_scoreReal + 0.5f);
+		_scoreText = "Score: " + (_scoreDisp.ToString().PadLeft(5, '0'));
+	}
+}
