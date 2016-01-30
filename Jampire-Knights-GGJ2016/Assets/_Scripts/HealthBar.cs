f63cb338d6ba62fc2b96408efde1277b9d8eb12a
s012721e - David Smith <david@flave.co.uk>
2016-01-30 00:51:21 +0000

    Added the Health UI elements with included scripts and Health Manager for the player. Health bar will decrease or increase depending on player health

---
+++ Jampire-Knights-GGJ2016/Assets/_Scripts/HealthBar.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    public Health healthManager;
+    Vector3 defaultScale;
+
+	// Use this for initialization
+	void Start () {
+        healthManager = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
+        defaultScale = new Vector3(gameObject.transform.localScale.x, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        adjustHealthBar();
+	}
+
+    void adjustHealthBar()
+    {
+        if (healthManager.health > 0)
+        {
+            gameObject.transform.localScale = new Vector3(defaultScale.x * (healthManager.health / healthManager.maxHealth), defaultScale.y, defaultScale.z);
+        }
+    }
+}
