625b466780f47f7930faf108d1b0c2f8aad68a00
s012721e - David Smith <david@flave.co.uk>
2016-01-30 03:22:07 +0000

    Frankensteined Bens old Enemy Spawner to spawn the items. Item spawn time is slower, appear further away and can appear anywhere within the given radius and the origin

---
+++ Jampire-Knights-GGJ2016/Assets/_Scripts/ItemSpawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemSpawner : MonoBehaviour {
+
+    public Transform itemPrefab;
+    public Transform itemOrigin;
+    public float radius;
+    float timer;
+
+	// Use this for initialization
+    void Start()
+    {
+        timer = 0;
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 5.0f)
+        {
+            timer -= 5.0f;
+            Vector3 pos = Random.onUnitSphere;
+            pos.y = 0;
+            //pos.Normalize();
+            pos *= radius;//on unit circle in xz plane with radius
+            Quaternion rot = Quaternion.LookRotation(-pos);//look towards the center when spawned, not important when enemy class controls rotation
+            Transform enemy = (Transform)Instantiate(itemPrefab);
+            enemy.parent = itemOrigin;
+            enemy.localPosition = pos;
+            enemy.rotation = rot;
+        }
+	}
+}
