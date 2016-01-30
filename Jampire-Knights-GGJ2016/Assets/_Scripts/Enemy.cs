10f3a4d40d905e73bd835da9be80f8f62fda2f6a
benjim <bencrawford101@gmail.com>
2016-01-30 00:57:00 +0000

    Added Enemy, EnemySpawner

---
+++ Jampire-Knights-GGJ2016/Assets/_Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    Vector3 targetPos;
+    Rigidbody rigidBody;
+
+	void Start()
+    {
+        targetPos = new Vector3(0, 0, 0);
+        //targetPos = GameObject.Find("Obelisk-Thing").transform.position;
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate()
+    {
+        Vector3 directionToMove = (targetPos - transform.position).normalized;
+        directionToMove.y = 0;
+        rigidBody.MovePosition(transform.position + directionToMove * speed * Time.deltaTime);
+	}
+}
---
+++ Jampire-Knights-GGJ2016/Assets/_Scripts/EnemySpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public float radius;
+    float timer;
+
+	void Start()
+    {
+        timer = 0;
+	}
+	
+	void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 1.0f)
+        {
+            timer -= 1.0f;
+            Vector3 pos = Random.onUnitSphere;
+            pos.y = 0;
+            pos.Normalize();
+            pos *= radius;//on unit circle in xz plane with radius
+            pos.y += 0.5f;//height of enemy (or we could put the enemies origin at its feet idk)
+            Quaternion rot = Quaternion.LookRotation(-pos);//look towards the center when spawned, not important when enemy class controls rotation
+            Instantiate(enemyPrefab, pos, rot);
+        }
+	}
+}
