6c2cd0a45ca8ad5b4504dc0c9d5ba341fd7386ea
s012721e - David Smith <david@flave.co.uk>
2016-01-31 04:18:03 +0000

    Added the first 10 tutorial waves for the player. Also added a ToolTip class which is able to show the player messages (As well as store a backlogue of competing messages)

---
+++ Jampire-Knights-GGJ2016/Assets/_Scripts/ToolTipMessage.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ToolTipMessage : MonoBehaviour {
+
+    private float timer = 0;
+    private string s;
+    private List<string> backlogMessages = new List<string>();
+    private List<float> backlogTimes = new List<float>();
+    public Text _tooltipText;
+
+    void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer <= 0)
+            {
+                timer = 0;
+                s = "";
+            }
+        }
+        else if (backlogMessages.Count > 0)
+        {
+            setUIMessage(backlogMessages[0], backlogTimes[0]);
+
+            backlogMessages.Remove(backlogMessages[0]);
+            backlogTimes.Remove(backlogTimes[0]);
+        }
+
+        _tooltipText.text = s;
+    }
+
+    public void setUIMessage(string message, float time)
+    {
+        // Do not fire message if another is displayed
+        if (timer == 0)
+        {
+            timer = time;
+            s = message;
+        }
+        else
+        {
+            backlogMessages.Add(message);
+            backlogTimes.Add(time);
+        }
+    }
+}
--- Jampire-Knights-GGJ2016/Assets/_Scripts/WaveManager.cs
+++ Jampire-Knights-GGJ2016/Assets/_Scripts/WaveManager.cs
@@ -15,45 +15,94 @@ public class WaveManager : MonoBehaviour
 	// Use this for initialization
 	void Awake ()
 	{
+        // First 10 waves are predetermined
         waves.Add(new Wave(enemySpawner, meteoriteSpawner, attackPatternDelay)
             .AddAttackPattern(3, 0, 5)
             .AddAttackPattern(4, 0, 5)
             .AddAttackPattern(8, 0, 5)
-
-            .AddAttackPattern(0, 3, 10)
-            .AddAttackPattern(0, 10, 10)
-            .AddAttackPattern(0, 300, 100)
-
-            .AddAttackPattern(20, 20, 20)
-            .AddAttackPattern(100, 100, 20)
-            .AddAttackPattern(2, 2, 1)
             );
+        // Second wave introduces meteors
         waves.Add(new Wave(enemySpawner, meteoriteSpawner, attackPatternDelay)
-            .AddAttackPattern(1, 1, 0.1f)
-            .AddAttackPattern(10, 10, 0.1f)
-            .AddAttackPattern(100, 100, 0.1f)
+            .AddAttackPattern(4, 1, 5)
+            .AddAttackPattern(8, 2, 5)
+            .AddAttackPattern(8, 3, 5)
             );
+        // Third wave increases the frequency of the meteors
         waves.Add(new Wave(enemySpawner, meteoriteSpawner, attackPatternDelay)
-            .AddAttackPattern(0, 3, 9)
-            .AddAttackPattern(4, 0, 3)
-            .AddAttackPattern(7, 20, 10)
+            .AddAttackPattern(8, 5, 6)
+            .AddAttackPattern(12, 5, 5)
+            .AddAttackPattern(12, 5, 5)
+            );
+        // Fourth wave forces the player to deal with a huge amount of enemies - defences need to be used more and more now
+        waves.Add(new Wave(enemySpawner, meteoriteSpawner, attackPatternDelay)
+            .AddAttackPattern(15, 8, 10)
+            .AddAttackPattern(20, 8, 15)
+            .AddAttackPattern(20, 8, 10)
+            );
+        // Fith wave enhances the previous wave by increasing speed
+        waves.Add(new Wave(enemySpawner, meteoriteSpawner, attackPatternDelay)
+            .AddAttackPattern(15, 8, 8)
+            .AddAttackPattern(20, 8, 12)
+            .AddAttackPattern(30, 8, 13)
+            );
+        // Sixth wave holds back a little on the players and focuses on meteorites
+        waves.Add(new Wave(enemySpawner, meteoriteSpawner, attackPatternDelay)
+            .AddAttackPattern(20, 10, 15)
+            .AddAttackPattern(20, 15, 15)
+            .AddAttackPattern(20, 20, 15)
+            );
+        // Seventh wave stops the meteor strikes and reapplies pressure with enemies
+        waves.Add(new Wave(enemySpawner, meteoriteSpawner, attackPatternDelay)
+            .AddAttackPattern(30, 0, 15)
+            .AddAttackPattern(30, 0, 15)
+            .AddAttackPattern(35, 0, 15)
+            );
+        // Eigth wave calms down and acts as a buffer before the final act
+        waves.Add(new Wave(enemySpawner, meteoriteSpawner, attackPatternDelay)
+            .AddAttackPattern(40, 10, 20)
+            .AddAttackPattern(40, 10, 20)
+            .AddAttackPattern(60, 10, 20)
+            );
+        // Ninth wave sends in increasingly hard waves
+        waves.Add(new Wave(enemySpawner, meteoriteSpawner, attackPatternDelay)
+            .AddAttackPattern(100, 5, 45)
+            .AddAttackPattern(120, 10, 50)
+            .AddAttackPattern(150, 15, 55)
+            );
+        // Tenth is the boss battle!
+        waves.Add(new Wave(enemySpawner, meteoriteSpawner, attackPatternDelay)
+            .AddAttackPattern(2500, 300, 350)
             );
 	}
 	
 	// Update is called once per frame
 	void Update()
-	{
+    {
+        ToolTipMessage toolTip = GameObject.FindGameObjectWithTag("ToolTip").GetComponent<ToolTipMessage>();
+
         if (waves[_waveIndex].GetWaveComplete())
 		{
             waves[_waveIndex]._waveActive = false;
-            Debug.Log("Wave " + _waveIndex.ToString() + " Complete");
+            toolTip.setUIMessage("Wave " + (_waveIndex + 1).ToString() + " Complete!", 3.0f);
+            Debug.Log("Wave " + (_waveIndex + 1).ToString() + " Complete");
             _waveIndex = (_waveIndex + 1) % waves.Count;
 		}
 
         if (waves[_waveIndex]._waveActive != true)
 		{
             waves[_waveIndex]._waveActive = true;
-			Debug.Log ("Wave " + _waveIndex.ToString() + " Active");
+            //toolTip.setUIMessage("Wave " + (_waveIndex + 1).ToString() + " Active", 3.0f);
+			Debug.Log ("Wave " + (_waveIndex + 1).ToString() + " Active");
+
+            // Special Cases for first few waves
+            if (_waveIndex == 0)
+            {
+                toolTip.setUIMessage("Defend the Ritualistic Obelisk from God's wrath!", 5.0f);
+            }
+            else if (_waveIndex == 1)
+            {
+                toolTip.setUIMessage("Right click to spend points to build defences", 5.0f);
+            }
 		}
 
         waves[_waveIndex].update();
